ee59fb60b007e7e4e697022eb971ed3482b29f8a
Oliwer1992 <164768075+Oliwer1992@users.noreply.github.com>
2025-04-17 12:24:14 +0200

    Przerobiłem na obiektowe i dodałem wstępne okno admina

--- login.py
+++ login.py
@@ -7,221 +7,269 @@ import sqlite3
 import bcrypt
 
 
-app = QApplication(sys.argv)
-
-
-def is_valid_email(email):
-    return '@' in email
-
-def is_valid_password(password):
-    return (len(password) >= 8 and
-    re.search(r'[A-Z]', password) and
-    re.search(r'\d', password) )
 
 
 
-def zarejestruj(window_2):
-    name = name_input.text()
-    email = mail_input.text()
-    login = login_input.text()
-    password = password_input.text()
+class LoginWindow(QWidget):
+    def __init__(self, register_window, main_window, admin_window):
+        super().__init__()
+        self.setWindowTitle("Ekran Logowania")
+        self.setGeometry(100, 100, 800, 600)
+        self.main_window = main_window
+        self.register_window = register_window
+        self.admin_window = admin_window
+        self.LoginUI()
 
-    if not name or not email or not login or not password:
-        QMessageBox.warning(window_2, "Nie podano wszystkich danych potrzebnych do logowania")
-        return False
+    def LoginUI(self):
+        label = QLabel("Login", self)
+        label.move(370, 100)
+        font = QFont()
+        font.setPointSize(17)
+        label.setFont(font)
 
+        self.login_logowanie = QLineEdit(self)
+        self.login_logowanie.move(300, 150)
+        self.login_logowanie.resize(200, 30)
 
+        label = QLabel("Password", self)
+        label.move(350, 250)
+        font = QFont()
+        font.setPointSize(17)
+        label.setFont(font)
 
-    if  not is_valid_email(email):
-        QMessageBox.warning(window_2, "Błąd", "podano niepoprawny adres email")
-        return False
-
-    if not is_valid_password(password):
-        QMessageBox.warning(window_2, "Błąd", "Hasło musi zawierać conajmniej 8 znaków oraz jedną dużą litere i jedną cyfre")
-        return False
-
-    password_crypted = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-    try:
-        conn = sqlite3.connect('project.db')
-        c = conn.cursor()
-
-        c.execute("""CREATE TABLE IF NOT EXISTS register (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT ,
-        email TEXT ,
-        login TEXT ,
-        password TEXT )
-        """)
-
-        c.execute("INSERT INTO register (name, email, login, password) VALUES (?,?,?,?)",(name,email,login,password_crypted))
-        conn.commit()
-        conn.close()
-        print("Konto założone!")
-        QMessageBox.information(window_2, "Sukces", "konto zostało założone!")
-        return True
-    except sqlite3.Error as error:
-        QMessageBox.warning(window_2,"Błąd!" ,"Użytkownik już istnieje")
-        return False
+        self.haslo_logowanie = QLineEdit(self)
+        self.haslo_logowanie.move(300, 300)
+        self.haslo_logowanie.resize(200, 30)
+        self.haslo_logowanie.setEchoMode(QLineEdit.EchoMode.Password)
 
 
-def register_success():
-    if zarejestruj(window_2):
-        window_2.hide()
-        window.show()
+        button = QPushButton("Login", self)
+        button.move(250, 500)
+        button.resize(100, 40)
+        button.clicked.connect(self.zaloguj)
 
+        button = QPushButton("Exit", self)
+        button.move(450, 500)
+        button.resize(100, 40)
+        button.clicked.connect(self.close)
 
-def zaloguj():
-    login = login_logowanie.text()
-    password = haslo_logowanie.text()
+        button_3 = QPushButton("Add new user", self)
+        button_3.move(350, 400)
+        button_3.resize(100, 40)
+        button_3.clicked.connect(self.go_to_register)
 
-    if not login or not password:
-        QMessageBox.warning(window, "Błąd", "Musisz podać login i hasło debilu !")
-        return False
+    def go_to_register(self):
+        self.hide()
+        self.register_window.show()
 
-    try:
-        conn = sqlite3.connect('project.db')
-        c = conn.cursor()
-        c.execute("SELECT password FROM register WHERE login = ?",(login,))
-        result = c.fetchone()
-        conn.close()
+    def zaloguj(self):
+        login = self.login_logowanie.text()
+        password = self.haslo_logowanie.text()
 
-        if result:
-            password_crypted = result[0]
-            if bcrypt.checkpw(password.encode('utf-8'), password_crypted):
-                QMessageBox.information(window, "Sukces", "Zalogowano pomyślnie!")
-                window.hide()
-                window_3.show()
-                return True
-            else:
-                QMessageBox.warning(window, "Błąd", "Nieprawidłowe hasłow")
-                return False
-        else:
-            QMessageBox.warning(window, "Błąd", "Nie znaleziono użytkownika")
+        if not login or not password:
+            QMessageBox.warning(self, "Błąd", "Musisz podać login i hasło debilu !")
             return False
-    except Exception as e:
-        QMessageBox.critical(window, "Błąd" , f"Wystąpił błąd: {e}")
+
+        try:
+            conn = sqlite3.connect('project.db')
+            c = conn.cursor()
+            c.execute("SELECT password FROM register WHERE login = ?", (login,))
+            result = c.fetchone()
+            conn.close()
+
+            if result:
+                password_crypted = result[0]
+                if bcrypt.checkpw(password.encode('utf-8'), password_crypted):
+                    QMessageBox.information(self, "Sukces", "Zalogowano pomyślnie!")
+                    self.hide()
+
+                    if login == "admin" and password == "Administrator1":
+                        self.admin_window.show()
+                    else:
+                        self.main_window.show()
+                    return True
+                else:
+                    QMessageBox.warning(self, "Błąd", "Nieprawidłowe hasłow")
+                    return False
+            else:
+                QMessageBox.warning(self, "Błąd", "Nie znaleziono użytkownika")
+                return False
+        except Exception as e:
+            QMessageBox.critical(self, "Błąd", f"Wystąpił błąd: {e}")
+
+
+class RegisterWindow(QWidget):
+    def __init__(self, login_window):
+        super().__init__()
+        self.setWindowTitle("Ekran Rejestracji")
+        self.setGeometry(100, 100, 800, 600)
+        self.RegisterUI()
+        self.login_window = login_window
+
+
+    def RegisterUI(self):
+
+        label = QLabel("Name", self)
+        label.move(370, 50)
+        font = QFont()
+        font.setPointSize(17)
+        label.setFont(font)
+
+
+        self.name_input = QLineEdit(self)
+        self.name_input.move(300, 100)
+        self.name_input.resize(200, 30)
+
+
+        label = QLabel("Email", self)
+        label.move(370, 150)
+        font = QFont()
+        font.setPointSize(17)
+        label.setFont(font)
+
+        self.mail_input = QLineEdit(self)
+        self.mail_input.move(300, 200)
+        self.mail_input.resize(200, 30)
+
+        label = QLabel("Login", self)
+        label.move(370, 250)
+        font = QFont()
+        font.setPointSize(17)
+        label.setFont(font)
+
+        self.login_input = QLineEdit(self)
+        self.login_input.move(300, 300)
+        self.login_input.resize(200, 30)
+
+        label = QLabel("Password", self)
+        label.move(350, 350)
+        font = QFont()
+        font.setPointSize(17)
+        label.setFont(font)
+
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+        self.password_input.move(300, 400)
+        self.password_input.resize(200, 30)
+
+        button = QPushButton("Add user", self)
+        button.move(250, 500)
+        button.resize(100, 40)
+        button.clicked.connect(self.register_success)
+
+        button = QPushButton("Back", self)
+        button.move(450, 500)
+        button.resize(100, 40)
+        button.clicked.connect(self.back_to_login)
+
+    def is_valid_email(self, email):
+        return '@' in email
+
+    def is_valid_password(self, password):
+        return (len(password) >= 8 and
+                re.search(r'[A-Z]', password) and
+                re.search(r'\d', password))
+
+    def zarejestruj(self):
+        name = self.name_input.text()
+        email = self.mail_input.text()
+        login = self.login_input.text()
+        password = self.password_input.text()
+
+        if not name or not email or not login or not password:
+            QMessageBox.warning(self, "Błąd","Nie podano wszystkich danych potrzebnych do logowania")
+            return False
+
+        if not self.is_valid_email(email):
+            QMessageBox.warning(self, "Błąd", "podano niepoprawny adres email")
+            return False
+
+        if not self.is_valid_password(password):
+            QMessageBox.warning(self, "Błąd",
+                                "Hasło musi zawierać conajmniej 8 znaków oraz jedną dużą litere i jedną cyfre")
+            return False
+
+        password_crypted = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+        try:
+            conn = sqlite3.connect('project.db')
+            c = conn.cursor()
+
+            c.execute("""CREATE TABLE IF NOT EXISTS register (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT ,
+            email TEXT UNIQUE ,
+            login TEXT UNIQUE ,
+            password TEXT )
+            """)
+
+            c.execute("SELECT 1 FROM register WHERE login = ? ",(login,))
+            if c.fetchone():
+                QMessageBox.warning(self, "Błąd", "Użytkownik o takim loginie już istnieje")
+                conn.close()
+                return False
+
+            c.execute("SELECT 1 FROM register WHERE email = ? ",(email,))
+            if c.fetchone():
+                QMessageBox.warning(self, "Bląd", "Użytkownik o takim adresu email już istnieje")
+                conn.close()
+                return False
+
+
+            c.execute("INSERT INTO register (name, email, login, password) VALUES (?,?,?,?)",
+                      (name, email, login, password_crypted))
+            conn.commit()
+            conn.close()
+            QMessageBox.information(self, "Sukces", "konto zostało założone!")
+            return True
+        except sqlite3.Error as error:
+            QMessageBox.warning(self, "Błąd!", f"Wystąpił błąd: {error}: ")
+            return False
+
+    def register_success(self):
+        if self.zarejestruj():
+            self.hide()
+            self.login_window.show()
+
+    def back_to_login(self):
+        self.hide()
+        self.login_window.show()
 
 
 
-# Ekran Logowania
-window = QWidget()
-window.setWindowTitle("Ekran Logowania")
-window.setGeometry(100, 100, 800, 600)
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Ekran Główny")
+        self.setGeometry(100, 100, 800, 600)
 
-# Ekran Rejestracji
-window_2 = QWidget()
-window_2.setWindowTitle("Ekran Rejestracji")
-window_2.setGeometry(100, 100, 800, 600)
-
-
-# Ekran Główny aplikacji ( testowy)
-window_3 = QMainWindow()
-window_3.setWindowTitle("Ekran Główny")
-window_3.setGeometry(100, 100, 800, 600)
-
-# Etykiety Rejestracja
-label = QLabel("Name",window_2)
-label.move(370,50)
-font = QFont()
-font.setPointSize(17)
-label.setFont(font)
-
-label = QLabel("Email",window_2)
-label.move(370,150)
-font = QFont()
-font.setPointSize(17)
-label.setFont(font)
-
-label = QLabel("Login",window_2)
-label.move(370,250)
-font = QFont()
-font.setPointSize(17)
-label.setFont(font)
-
-label = QLabel("Password",window_2)
-label.move(350,350)
-font = QFont()
-font.setPointSize(17)
-label.setFont(font)
-
-#Etykiety Logowanie
-label = QLabel("Login",window)
-label.move(370,100)
-font = QFont()
-font.setPointSize(17)
-label.setFont(font)
-
-label = QLabel("Password",window)
-label.move(350,250)
-font = QFont()
-font.setPointSize(17)
-label.setFont(font)
-
-# Pole tekstowe Logowanie
-#Login
-login_logowanie = QLineEdit(window)
-login_logowanie.move(300, 150)
-login_logowanie.resize(200, 30)
-# Hasło
-haslo_logowanie = QLineEdit(window)
-haslo_logowanie.move(300, 300)
-haslo_logowanie.resize(200, 30)
-haslo_logowanie.setEchoMode(QLineEdit.EchoMode.Password)
-
-# Pola tekstowe Rejestracja
-# Imię
-name_input = QLineEdit(window_2)
-name_input.move(300, 100)
-name_input.resize(200, 30)
-# Email
-mail_input = QLineEdit(window_2)
-mail_input.move(300, 200)
-mail_input.resize(200, 30)
-#Login
-login_input = QLineEdit(window_2)
-login_input.move(300, 300)
-login_input.resize(200, 30)
-#Hasło
-password_input = QLineEdit(window_2)
-password_input.setEchoMode(QLineEdit.EchoMode.Password)
-password_input.move(300, 400)
-password_input.resize(200, 30)
-
-# Przyciski Rejestracja
-
-button = QPushButton("Add user", window_2)
-button.move(250, 500)
-button.resize(100, 40)
-button.clicked.connect(register_success)
+class AdminWindow(MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Ekran Administrator")
+        self.setGeometry(100, 100, 800, 600)
 
 
 
-button = QPushButton("Back", window_2)
-button.move(450, 500)
-button.resize(100, 40)
-button.clicked.connect(window.show)
-button.clicked.connect(window_2.hide)
 
-# Przycisk Logowanie
-button = QPushButton("Login", window)
-button.move(250, 500)
-button.resize(100, 40)
-button.clicked.connect(zaloguj)
 
-button = QPushButton("Exit", window)
-button.move(450, 500)
-button.resize(100, 40)
-button.clicked.connect(window.close)
 
-button_3 = QPushButton("Add new user", window)
-button_3.move(350, 400)
-button_3.resize(100, 40)
-button_3.clicked.connect(window_2.show)
-button_3.clicked.connect(window.hide)
 
-window.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    admin_window = AdminWindow()
+
+    register_window = RegisterWindow(None)  # tymczasowo bez login_window
+    login_window = LoginWindow(register_window=register_window, main_window=main_window, admin_window=admin_window)
+    register_window.login_window = login_window  # poprawne przypisanie
+
+    login_window.show()
+    sys.exit(app.exec())
+
+
+
+
 
 
 
